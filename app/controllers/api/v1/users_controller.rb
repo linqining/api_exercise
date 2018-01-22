29339bc7965e12218ded9129b7fef5f60a2f615f
linqining <linqining@outlook.com>
2018-01-22 17:14:51 +0800

    add logo

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,13 +15,14 @@ class Api::V1::UsersController < ApiController
  def user_params
    params.permit(:email, :password, :avatar)
  end
-def show
- render :json => {
-   :email => current_user.email,
-   :avatar => current_user.avatar,
-   :updated_at => current_user.updated_at,
-   :created_at => current_user.created_at
- }
-end
+
+  def show
+   render :json => {
+     :email => current_user.email,
+     :avatar => current_user.avatar,
+     :updated_at => current_user.updated_at,
+     :created_at => current_user.created_at
+   }
+  end
 
 end
